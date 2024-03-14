3000a852c9cc3ff16f977b74eb7073bd41e9dd3d
danielcastro0310 <daniel.espinosa03@uptc.edu.co>
2024-03-14 18:23:12 -0500

    se actualiza persistencia en proceso de crear conexiones para cada tipo de usuario

--- co.edu.uptcSoft.proyecro2do50Fx/src/main/java/Persistence/Persistence.java
+++ co.edu.uptcSoft.proyecro2do50Fx/src/main/java/Persistence/Persistence.java
@@ -1,19 +1,30 @@
 package Persistence;
 
+import model.Charge;
+import model.Company;
+import model.Oferent;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Persistence {
-   public void write(ArrayList<String> objeto) throws IOException {
-       ObjectOutputStream write = new ObjectOutputStream(new FileOutputStream("C:\\Users\\angel\\OneDrive\\Documentos\\ProyectDocuments\\documents.dat"));
-       write.writeObject(objeto);
+    //private ArrayList<Oferent> oferents = new ArrayList<>();
+   // private ArrayList<Company> companies = new ArrayList<>();
+    //private Map<String, ArrayList<Charge>> userCompany = new HashMap<>();
+    //private Map<String, ArrayList<Charge>> userTransport = new HashMap<>();
+
+    public void writeOferent(ArrayList<Oferent> oferent) throws IOException {
+       ObjectOutputStream write = new ObjectOutputStream(new FileOutputStream("C:\\Users\\angel\\OneDrive\\Documentos\\ProyectDocuments\\oferents.dat"));
+       write.writeObject(oferent);
        write.close();
    }
-   public ArrayList<String> read() throws IOException, ClassNotFoundException {
-       ObjectInputStream read = new ObjectInputStream(new FileInputStream("C:\\Users\\angel\\OneDrive\\Documentos\\ProyectDocuments\\documents.dat"));
-  ArrayList<String> objeto= (ArrayList<String>) read.readObject();
+   public ArrayList<Oferent> readoferent() throws IOException, ClassNotFoundException {
+       ObjectInputStream read = new ObjectInputStream(new FileInputStream("C:\\Users\\angel\\OneDrive\\Documentos\\ProyectDocuments\\oferents.dat"));
+  ArrayList<Oferent> oferent= (ArrayList<Oferent>) read.readObject();
   read.close();
-  return objeto;
+  return oferent;
    }
 
 }
