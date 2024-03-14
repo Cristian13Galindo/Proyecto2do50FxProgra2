e84ef00e9d99e5915139fe1c8ed5589b08169c15
danielcastro0310 <daniel.espinosa03@uptc.edu.co>
2024-03-14 18:27:38 -0500

    avances en persistencia metodos Company

--- co.edu.uptcSoft.proyecro2do50Fx/src/main/java/Persistence/Persistence.java
+++ co.edu.uptcSoft.proyecro2do50Fx/src/main/java/Persistence/Persistence.java
@@ -10,8 +10,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Persistence {
-    //private ArrayList<Oferent> oferents = new ArrayList<>();
-   // private ArrayList<Company> companies = new ArrayList<>();
     //private Map<String, ArrayList<Charge>> userCompany = new HashMap<>();
     //private Map<String, ArrayList<Charge>> userTransport = new HashMap<>();
 
@@ -26,5 +24,17 @@ public class Persistence {
   read.close();
   return oferent;
    }
+
+    public void writeCompany(ArrayList<Company> company) throws IOException {
+        ObjectOutputStream write = new ObjectOutputStream(new FileOutputStream("C:\\Users\\angel\\OneDrive\\Documentos\\ProyectDocuments\\companys.dat"));
+        write.writeObject(company);
+        write.close();
+    }
+    public ArrayList<Company> readCompany() throws IOException, ClassNotFoundException {
+        ObjectInputStream read = new ObjectInputStream(new FileInputStream("C:\\Users\\angel\\OneDrive\\Documentos\\ProyectDocuments\\companys.dat"));
+        ArrayList<Company> company= (ArrayList<Company>) read.readObject();
+        read.close();
+        return company;
+    }
 
 }
